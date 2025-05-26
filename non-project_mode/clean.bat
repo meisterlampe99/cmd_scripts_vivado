f9ba6d8f13a03318154a4346a1fc074095abee44
PatrickL <patrick.lampl@fh-joanneum.at>
2025-05-26 14:23:47 +0200

    merged: arg. requirement removed from clean

--- non-project_mode/clean.bat
+++ non-project_mode/clean.bat
@@ -1,15 +1,5 @@
 
 @echo off
-<<<<<<< HEAD
-=======
-REM Check if an argument was provided
-if "%~1"=="" (
-    echo Usage: runBit.bat TopModuleName
-    exit /b 1
-)
-
-set TopModuleName=%~1
->>>>>>> 9755c2349aa89c852daea603d0abcb43e06c75c1
 
 REM clean up working directory
 DEL vivado*.zip
@@ -22,3 +12,4 @@ RMDIR /S /Q outputBit
 RMDIR /S /Q .Xil
 ECHO ------------------------------------
 ECHO CMDscripts4Vivado: all (or most) output files, folders and vivado logs removed
+ 
