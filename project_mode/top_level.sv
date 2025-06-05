80090ae458f2c3c45e4f8fb1a70d05b2f13c1fcd
PatrickL <patrick.lampl@fh-joanneum.at>
2025-06-05 18:10:21 +0200

    added reset synchronizer to example design

--- project_mode/top_level.sv
+++ project_mode/top_level.sv
@@ -4,6 +4,23 @@ module top_level(
     input wire clk, rst, 
     output logic [7:0] leds
     );
+
+//internal variables
+logic rst_s;
+logic [1:0] rst_sreg;
+
+//supply based reset synchronizer
+always_ff @(posedge clk, posedge rst)
+begin
+    if (rst)
+        //assert internal reset asynchronously
+        rst_sreg <= 2'b11;
+    else
+        //deassert internal reset synchronously
+        rst_sreg <= {rst_sreg[0], 1'b0};
+end
+//assign synchronized reset from rst_sreg
+always_comb rst_s = rst_sreg[1];
     
 //variable for storing count value
 logic [23:0] cnt;
@@ -11,7 +28,7 @@ logic [23:0] cnt;
 //counter itself
 always_ff @(posedge clk)
 begin
-    if (rst)
+    if (rst_s)
         cnt <= 0;
     else
         cnt <= cnt + 1;
