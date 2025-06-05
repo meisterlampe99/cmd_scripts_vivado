b5fdfc34385c10ef2a67aba4b69746f7d507a0e6
PatrickL <patrick.lampl@fh-joanneum.at>
2025-06-05 18:09:36 +0200

    fixed clean.bat of project mode

--- project_mode/clean.bat
+++ project_mode/clean.bat
@@ -1,12 +1,5 @@
 
 @echo off
-REM Check if an argument was provided
-if "%~1"=="" (
-    echo Usage: runBit.bat TopModuleName
-    exit /b 1
-)
-
-set TopModuleName=%~1
 
 REM clean up working directory
 DEL vivado*.zip
